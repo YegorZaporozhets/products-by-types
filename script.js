4ac2072f8bc4e768dba0b21d47f653a26df89714
Yegor Zaporozhets <ezaporozhets@oggettoweb.com>
2019-02-15 16:43:01 +0300

    Add sort for product's types and render, add simple CSS for products

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 'use strict';
 $(function () {
-    // Ваш код
+    $.fancybox.showLoading();
+    $.getJSON('products.json', products => {
+        const $sale = $('#sale');
+        const $promo = $('#promo');
+        const $recommended = $('#recommended');
+        const $productTemplate = $('#productTemplate');
+        const PRODUCTS_TEMPLATE = _.template($productTemplate.html());
+        const PRODUCTS = products;
+
+        const PRODUCTS_SALE = {
+            title: 'Распродажа',
+            items: PRODUCTS.filter( product => product.type === 'sale')
+        };
+
+        const PRODUCTS_PROMO = {
+            title: 'Промо-акция',
+            items: PRODUCTS.filter( product => product.type === 'promo')
+        };
+
+        const PRODUCTS_RECOMMENDED = {
+            title: 'Рекомендуемые товары',
+            items: PRODUCTS.filter( product => product.type === 'recommended')
+        };
+
+
+
+        $sale.html(PRODUCTS_TEMPLATE(PRODUCTS_SALE));
+        $promo.html(PRODUCTS_TEMPLATE(PRODUCTS_PROMO));
+        $recommended.html(PRODUCTS_TEMPLATE(PRODUCTS_RECOMMENDED));
+    })
+        .fail((e) => {
+            console.log('Ошибка', e.status, e.statusText);
+        })
+        .always(() => {
+            $.fancybox.hideLoading();
+        });
 });
